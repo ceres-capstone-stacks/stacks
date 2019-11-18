ef96d4333e751c032a7c40af8f2cded816bdd509
Eddy Bautista <bautista.j.eddy@gmail.com>
2019-11-18 15:40:36 -0600

    adding gitignore

---
+++ src/main/java/com/stack/stacks/StacksApplication.java
@@ -0,0 +1,13 @@
+package com.stack.stacks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StacksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StacksApplication.class, args);
+    }
+
+}
