3898ad74b77a821d56a4cdbacd0aeff1fe7505bc
Jason Kempel <kempel.jason13@gmail.com>
2019-11-20 11:01:03 -0600

    created authentication controller to handle login, create user functional

--- src/main/java/com/stack/stacks/controller/UsersController.java
+++ src/main/java/com/stack/stacks/controller/UsersController.java
@@ -1,20 +1,37 @@
 package com.stack.stacks.controller;
 
+import com.stack.stacks.models.User;
+import com.stack.stacks.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UsersController {
+    private UserRepository userDao;
 
-    @GetMapping("/login")
-    public String showLogin(){
-        return "users/login";
+    public UsersController(UserRepository userDao){
+        this.userDao = userDao;
     }
+//    @GetMapping("/login")
+//    public String showLogin(){
+//        return "users/login";
+//    }
 
     @GetMapping("/register")
-    public String showRegisterPage(){
+    public String showRegisterPage(Model vmModel){
+        vmModel.addAttribute("user", new User());
         return "users/register";
+    }
+    @PostMapping("/register")
+    public String saveUser(@ModelAttribute User user){
+//        String hash = passwordEncoder.encode(user.getPassword());
+//        user.setPassword(hash);
+        userDao.save(user);
+        System.out.println("This is registering something");
+        return "redirect:/login";
     }
 
     @GetMapping("/profile")
