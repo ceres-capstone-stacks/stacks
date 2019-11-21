22987ba64595c09b266e64efef553a6f13bac51e
Eddy Bautista <bautista.j.eddy@gmail.com>
2019-11-21 16:34:03 -0600

    post created

--- src/main/java/com/stack/stacks/controller/PostsController.java
+++ src/main/java/com/stack/stacks/controller/PostsController.java
@@ -1,21 +1,52 @@
 package com.stack.stacks.controller;
 
+import com.stack.stacks.models.Post;
+import com.stack.stacks.models.User;
+import com.stack.stacks.repositories.PostRepository;
+import com.stack.stacks.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PostsController {
 
+    private final PostRepository postDao;
+    private final UserRepository userDao;
+
+
+    public PostsController(PostRepository postDao, UserRepository userDao) {
+        this.postDao = postDao;
+        this.userDao = userDao;
+    }
+
     @GetMapping("/posts")
-    public String showPosts(){
+    public String showPosts(Model x){
+        x.addAttribute("posts", postDao.findAll());
         return "posts/index";
     }
 
     @GetMapping("/posts/create")
-    public String createPosts(){
+    public String showCreateForm(Model model) {
+        model.addAttribute("post", new Post());
         return "posts/create";
     }
+
+
+
+    @PostMapping("/posts/create")
+    public String create(@ModelAttribute Post postToBeCreated) {
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        postToBeCreated.setUser(currentUser);
+        postDao.save(postToBeCreated);
+
+        return "redirect:/posts/index";
+
+    }
+
 
     @GetMapping("/posts/myposts")
     public String usersPosts(){
