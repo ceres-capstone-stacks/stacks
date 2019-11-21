7084d55b5e3a6be553b49f3e566319001767d787
Jason Kempel <kempel.jason13@gmail.com>
2019-11-21 16:19:38 -0600

    utilizing seeder instead of console

---
+++ seeder.sql
@@ -0,0 +1,5 @@
+
+insert into expenses (amount, date, description, is_regular, user_id) values ('9.91', '2019-08-30', 'Opn/oth rep tcspd vlv-ts', false, 1);
+insert into expenses (amount, date, description, is_regular, user_id) values ('9.29', '2019-03-14', 'Delay opening enterostom', true, 1);
+
+
