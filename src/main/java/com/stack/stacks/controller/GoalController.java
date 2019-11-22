7fe5d43aa101c065583328ea4f02e5cc979d0699
Jason Kempel <kempel.jason13@gmail.com>
2019-11-22 09:17:49 -0600

    working on goals

--- src/main/java/com/stack/stacks/controller/GoalController.java
+++ src/main/java/com/stack/stacks/controller/GoalController.java
@@ -4,10 +4,13 @@ import com.stack.stacks.models.Goal;
 import com.stack.stacks.models.User;
 import com.stack.stacks.repositories.ExpenseRepository;
 import com.stack.stacks.repositories.GoalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -15,19 +18,39 @@ public class GoalController {
 
     private GoalRepository goalDao;
 
-    public GoalController (GoalRepository goalDao){
+
+    public GoalController(GoalRepository goalDao) {
         this.goalDao = goalDao;
     }
 
     @GetMapping("/goals")
-    public String showGoalsIndex (Model vModel) {
-        vModel.addAttribute("goal", new Goal());
+    public String showGoals(Model vModel) {
+        vModel.addAttribute(goalDao.findAll());
         return "/goals/index";
     }
 
-    @PostMapping ("/goals")
-    public String createGoal (@ModelAttribute Goal goal) {
-        goalDao.save(goal);
-        return "/goals/create";
+    @GetMapping("/goals/create")
+    public String showGoalsIndex(Model vModel) {
+        vModel.addAttribute("goal", new Goal());
+        return "/goals/index";
     }
+    @PostMapping("/goals/create")
+    public String create(@ModelAttribute Goal goalToBeCreated){
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        goalToBeCreated.setUser(currentUser);
+        Goal savedGoal = goalDao.save(goalToBeCreated);
+        return "redirect:/goals/" + savedGoal.getId();
+    }
+
+    @GetMapping("/goals/{id}")
+    public String show(@PathVariable long id, Model vModel) {
+        vModel.addAttribute("goal", goalDao.getOne(id));
+        return "goals/show";
+    }
+
+//    @PostMapping("/goals/create")
+//    public String createGoal(@ModelAttribute Goal goal) {
+//        goalDao.save(goal);
+//        return "/goals/index";
+//    }
 }
