2e8736e4395fd97be63f5067456622a7e40eaa40
Jason Kempel <kempel.jason13@gmail.com>
2019-11-24 22:42:51 -0600

    working on edit/delete goals

--- src/main/java/com/stack/stacks/controller/GoalController.java
+++ src/main/java/com/stack/stacks/controller/GoalController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -31,24 +28,30 @@ public class GoalController {
     }
 
     @GetMapping("/goals")
-    public String showGoals(Model vModel) {
-        List<Goal> goalsList = goalDao.findAll();
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-
-        for(Goal aGoal : goalsList ) {
-            String strDate= formatter.format(aGoal.getDate());
-            Date date1= null;
-            try {
-                date1 = new SimpleDateFormat("MM/dd/yyyy").parse(strDate);
-            } catch (ParseException e) {
-                System.out.println("invalid date format in goalsList");
-                break;
-            }
-            aGoal.setDate(date1);
-        }
-        vModel.addAttribute("goal", goalsList);
+    public String index(Model vModel) {
+        vModel.addAttribute("goals", goalDao.findAll());
         return "goals/index";
     }
+
+//    @GetMapping("/goals")
+//    public String showGoals(Model vModel) {
+//        List<Goal> goalsList = goalDao.findAll();
+//        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+//
+//        for(Goal aGoal : goalsList ) {
+//            String strDate= formatter.format(aGoal.getDate());
+//            Date date1= null;
+//            try {
+//                date1 = new SimpleDateFormat("MM/dd/yyyy").parse(strDate);
+//            } catch (ParseException e) {
+//                System.out.println("invalid date format in goalsList");
+//                break;
+//            }
+//            aGoal.setDate(date1);
+//        }
+//        vModel.addAttribute("goal", goalsList);
+//        return "goals/index";
+//    }
 
     @GetMapping("/goals/create")
     public String showGoalsIndex(Model vModel) {
@@ -69,8 +72,17 @@ public class GoalController {
 
     @GetMapping("/goals/{id}/edit")
     public String editPost(@PathVariable long id, Model vModel) {
-        vModel.addAttribute("goal", goalDao.getOne(id));
+        vModel.addAttribute("goals", goalDao.getOne(id));
         return "goals/edit";
+    }
+
+    @PostMapping("/goals/{id}/edit")
+    public String update(@PathVariable long id, @RequestParam Double amountSaved, @RequestParam Date date) {
+        Goal oldGoal = goalDao.getOne(id);
+        oldGoal.setAmountSaved(amountSaved);
+        oldGoal.setDate(date);
+        goalDao.save(oldGoal);
+        return "redirect:/goals/" + id;
     }
 
 //    @PostMapping("/goals/create")
@@ -78,4 +90,8 @@ public class GoalController {
 //        goalDao.save(goal);
 //        return "/goals/index";
 //    }
+
+    public GoalController() {
+        super();
+    }
 }
