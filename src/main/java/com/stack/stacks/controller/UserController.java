80da7c1697ad7809c3e29c9fef1aed9f8c13820f
Eddy Bautista <bautista.j.eddy@gmail.com>
2019-12-02 15:55:32 -0600

    refactor on exp

--- src/main/java/com/stack/stacks/controller/UserController.java
+++ src/main/java/com/stack/stacks/controller/UserController.java
@@ -9,10 +9,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,7 +43,7 @@ public class UserController {
     public String showProfile(Model vModel){
         User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         List<Expense> allExpenses = expenseDao.findAll();
-        Long sumOfExpenses = expenseDao.sumOfExpenses();
+        Double sumOfExpenses = expenseDao.sumOfExpenses(loggedInUser.getId());
         List<Expense> expenses = new ArrayList<>();
         System.out.println(sumOfExpenses);
         for(Expense expense : allExpenses){
@@ -66,23 +63,23 @@ public class UserController {
 
 
 
-    @GetMapping("/profile/expenses")
-    public String getExpenses(Model vModel) {
-        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        List<Expense> allExpenses = expenseDao.findAll();
-//        List<Expense> sumOfExpenses = expenseDao.sumOfExpenses();
-        List<Expense> expenses = new ArrayList<>();
-        for(Expense expense : allExpenses){
-            if(expense.getUser() != null) {
-                if (expense.getUser().getId() == currentUser.getId()) {
-                    expenses.add(expense);
-                }
-            }
-        }
-        vModel.addAttribute("expense", new Expense());
-        vModel.addAttribute("expenses", expenses);
-        return "expenses/index";
-    }
+//    @GetMapping("/profile/expenses")
+//    public String getExpenses(Model vModel) {
+//        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        List<Expense> allExpenses = expenseDao.findAll();
+////        List<Expense> sumOfExpenses = expenseDao.sumOfExpenses();
+//        List<Expense> expenses = new ArrayList<>();
+//        for(Expense expense : allExpenses){
+//            if(expense.getUser() != null) {
+//                if (expense.getUser().getId() == currentUser.getId()) {
+//                    expenses.add(expense);
+//                }
+//            }
+//        }
+//        vModel.addAttribute("expense", new Expense());
+//        vModel.addAttribute("expenses", expenses);
+//        return "expenses/index";
+//    }
 
     @GetMapping("/profile/expenses/create")
     public String showCreateForm(Model model) {
