d6c4e20c5a0175d4e446ec7522e127e0782a4954
Jason Kempel <kempel.jason13@gmail.com>
2019-12-02 13:39:26 -0600

    updated the error 500 page

--- seeder.sql
+++ seeder.sql
@@ -2,7 +2,7 @@ insert into expenses (amount, date, description, is_regular, type, user_id) valu
 insert into expenses (amount, date, description, is_regular, type, user_id) values ('9.29', '2019-03-14', 'Delay opening enterostom', true, 2, 1);
 
 
-insert into goals (amount, amount_saved, date, description, user_id) values ('10.01', '4.55', '2021-01-01','this is a savings plan',1);
+insert into goals (amount, amount_saved, date, description, user_id) values ('10.00', '4.55', '2021-01-01','this is a savings plan',1);
 insert into goals (amount, amount_saved, date, description, user_id) values ('250.00', '25.55', '2020-11-12','this is for my birthday',1);
 
 TRUNCATE goals;
